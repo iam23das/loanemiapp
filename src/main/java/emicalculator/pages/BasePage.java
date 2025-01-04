cf25728236b471626798eab479dab1d09cf01bc2
iam23das <abhishek.das.primary@gmail.com>
2025-01-05 00:57:38 +0530

    changed basepage and added .gitignore

--- src/main/java/emicalculator/pages/BasePage.java
+++ src/main/java/emicalculator/pages/BasePage.java
@@ -5,41 +5,48 @@ import org.openqa.selenium.WebDriver;
 
 import emicalculator.driverCenter.WebElementFacade;
 
-public class HomeLoan implements LoanPage{
-	private WebElementFacade homeLoanAmountElement;
+public abstract class BasePage implements LoanPage {
+	private WebElementFacade loanAmountElement;
 	private WebElementFacade interestRateAmountElement;
 	private WebElementFacade loanTenureValueElement;
 	private WebElementFacade loanEmiAmountElement;
 	private WebElementFacade totalInterestPayableElement;
 	private WebElementFacade totalPaymentElement;
-	private WebElementFacade yearOrMnthElement;
+	private WebElementFacade yearElement;
+	private WebElementFacade monthElement;
 
-	public HomeLoan(WebDriver driver) {
-		homeLoanAmountElement = new WebElementFacade(driver, By.id("loanamount"));
+	public BasePage(WebDriver driver) {
+		loanAmountElement = new WebElementFacade(driver, By.id("loanamount"));
 		interestRateAmountElement = new WebElementFacade(driver, By.id("loaninterest"));
+		loanTenureValueElement = new WebElementFacade(driver, By.id("loanterm"));
+		loanEmiAmountElement = new WebElementFacade(driver, By.xpath("//div[@id='emiamount']/p/span"));
+		totalInterestPayableElement = new WebElementFacade(driver, By.id("//div[@id='emitotalinterest']/p/span"));
+		totalPaymentElement = new WebElementFacade(driver, By.xpath("//div[@id='emitotalamount']/p/span"));
 	}
+	
+
 	@Override
 	public void enterLoanAmount() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enterInterestRate() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enterLoanTenureInYear() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enterLoanTenureInMonths() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -59,5 +66,5 @@ public class HomeLoan implements LoanPage{
 		// TODO Auto-generated method stub
 		return null;
 	}
-    
+
 }
