3af93c9798311f9b55fdf1526d70df4c648c5181
iam23das <abhishek.das.primary@gmail.com>
2025-01-07 00:08:33 +0530

    changed

--- src/test/java/emicalculator/unittests/CheckingPropertyLoadFunction.java
+++ src/test/java/emicalculator/unittests/CheckingPropertyLoadFunction.java
@@ -1,12 +1,15 @@
 package emicalculator.unittests;
 
 import emicalculator.utility.PropertyUtils;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CheckingPropertyLoadFunction {
     @Test
     public void testPropertyLoad(){
+    	System.out.println(PropertyUtils.getProperty("chrome.driver.path"));
         Assert.assertEquals(PropertyUtils.getProperty("chrome.driver.path"), "D:\\WebDriver\\chromedriver.exe");
     }
 }
